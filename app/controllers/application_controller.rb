fe27fccd980a56bc5ae51cf594e6cdfdbbb2f599
Nikolai Zhukovets <7nz.zn7@gmail.com>
2023-01-06 01:27:26 -0600

    feat: secure endpoints

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  def authorize_request
+    return if request[:access_key] == ENV.fetch('APP_API_KEY')
+
+    render json: { errors: 'Access denied.' }, status: :unauthorized
+  end
 end
--- app/controllers/geolocations_controller.rb
+++ app/controllers/geolocations_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class GeolocationsController < ApplicationController
+  before_action :authorize_request
   before_action :set_geolocation, only: %i[show destroy]
 
   # GET /geolocation/1
